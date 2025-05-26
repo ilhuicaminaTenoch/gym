f2c3042ae18eea85df6e7cb28acc461719bc025f
Manuel Moreno <jmmoreno@rotoplas.com>
2025-05-25 20:21:32 -0600

    Se modifica producto y se regularizan las migraciones

--- target/classes/db/migration/V2__agrega_estatus_producto.sql
+++
@@ -1,2 +0,0 @@
-alter table producto
-    add estatus varchar(50);
--- target/classes/db/migration/V3__sku_unico_producto.sql
+++
@@ -1,5 +0,0 @@
-alter table producto
-    alter column sku set not null;
-
-create unique index producto__index__sku
-    on producto (sku);
